4d9b20c014d8c40fbae5e131071f2c5bc060c4fa
Alona <103571003+ARomanii@users.noreply.github.com>
2022-10-27 17:28:27 +0300

    ДЗ 2 (1)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+
+const App = () => {
+  <>
+  <h1>Сщстояние компонента</h1>
+
+  <Counter />
+  </>
 };
+
+export default App;
